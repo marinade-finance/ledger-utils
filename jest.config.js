f964a59b04e89f2bcdaa222b0826d4a825ab01a3
chalda <chalda@marinade.finance>
2025-09-23 13:46:32 +0200

    [dependencies] @marinade.finance libraries refactor [GEN-6320] (#4)
    * [dependencies] @marinade.finance libraries refactor
    
    * Update packages/ledger-utils/package.json
    
    Co-authored-by: martinkrecek <9853061+martinkrecek@users.noreply.github.com>
    
    * [fix] prettier ignore
    
    * dependencies
    
    ---------
    
    Co-authored-by: martinkrecek <9853061+martinkrecek@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/build/'],
   testTimeout: 120000,
-  testPathIgnorePatterns: ['__tests__/.*.skip.ts'],
   detectOpenHandles: true,
-  setupFilesAfterEnv: [
-    /// https://github.com/marinade-finance/marinade-ts-cli/blob/main/packages/lib/jest-utils/src/equalityTesters.ts
-    '<rootDir>/node_modules/@marinade.finance/jest-utils/src/equalityTesters',
-  ],
+  modulePathIgnorePatterns: ['dist/'],
+  testPathIgnorePatterns: ['__tests__/.*.skip.ts'],
 }
